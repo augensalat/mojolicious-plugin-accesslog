84181bc2c4826e450a0b184c24ea66c2dfd36597
Bernhard Graf <augensalat@gmail.com>
2015-06-19 17:41:03 +0200

    Bump version to 0.009.

--- lib/Mojolicious/Plugin/AccessLog.pm
+++ lib/Mojolicious/Plugin/AccessLog.pm
@@ -10,7 +10,7 @@ use Scalar::Util qw(blessed reftype weaken);
 use Socket qw(inet_aton AF_INET);
 use Time::HiRes qw(gettimeofday tv_interval);
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 my $DEFAULT_FORMAT = 'common';
 my %FORMATS = (
@@ -281,7 +281,7 @@ Mojolicious::Plugin::AccessLog - An AccessLog Plugin for Mojolicious
 
 =head1 VERSION
 
-Version 0.008
+Version 0.009
 
 =head1 SYNOPSIS
 
